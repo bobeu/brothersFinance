bb67100cdda0fdb14f9993dc799245b72705a0c7
bobeu <bobmatea27@gmail.com>
2021-06-29 08:48:54 +0100

    updated repository fields

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,13 @@
-// var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var PeerBrothers = artifacts.require("./PeerBrothers.sol");
+// var web3 = artifacts.require("Web3");
 
-// module.exports = function(deployer) {
-//   deployer.deploy(SimpleStorage);
-// };
+// async () => accounts = await web3.eth.getAccounts();
+module.exports = function(deployer) {
+  deployer.deploy(PeerBrothers, 299999999, '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56');
+};
 
-
-
+// 299999999, 0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56
+// {gas: 9612388, gasLimit: 9612388, from: "0x236CF87220Fef3219104b5fF624d40CB880D6b9B"}
 
 
 
@@ -45,34 +47,34 @@
 // deploy_web3
 
 // Right click on the script name and hit "Run" to execute
-(async () => {
-  try {
-      console.log('Running deployWithWeb3 script...')
+// (async () => {
+//   try {
+//       console.log('Running deployWithWeb3 script...')
       
-      const contractName = 'PeerBrothers' // Change this for other contract
-      const constructorArgs = [299999999, 0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56]    // Put constructor args (if any) here for your contract
+//       const contractName = 'PeerBrothers' // Change this for other contract
+//       const constructorArgs = [299999999, 0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56]    // Put constructor args (if any) here for your contract
   
-      // Note that the script needs the ABI which is generated from the compilation artifact.
-      // Make sure contract is compiled and artifacts are generated
-      const artifactsPath = `browser/contracts/artifacts/${contractName}.json` // Change this for different path
+//       // Note that the script needs the ABI which is generated from the compilation artifact.
+//       // Make sure contract is compiled and artifacts are generated
+//       const artifactsPath = `browser/contracts/artifacts/${contractName}.json` // Change this for different path
 
-      const metadata = JSON.parse(await remix.call('fileManager', 'getFile', artifactsPath))
-      const accounts = await web3.eth.getAccounts()
+//       const metadata = JSON.parse(await remix.call('fileManager', 'getFile', artifactsPath))
+//       const accounts = await web3.eth.getAccounts()
   
-      let contract = new web3.eth.Contract(metadata.abi)
+//       let contract = new web3.eth.Contract(metadata.abi)
   
-      contract = contract.deploy({
-          data: metadata.data.bytecode.object,
-          arguments: constructorArgs
-      })
+//       contract = contract.deploy({
+//           data: metadata.data.bytecode.object,
+//           arguments: constructorArgs
+//       })
   
-      const newContractInstance = await contract.send({
-          from: accounts[0],
-          gas: 1500000,
-          gasPrice: '30000000000'
-      })
-      console.log('Contract deployed at address: ', newContractInstance.options.address)
-  } catch (e) {
-      console.log(e.message)
-  }
-})()
+//       const newContractInstance = await contract.send({
+//           from: accounts[0],
+//           gas: 1500000,
+//           gasPrice: '30000000000'
+//       })
+//       console.log('Contract deployed at address: ', newContractInstance.options.address)
+//   } catch (e) {
+//       console.log(e.message)
+//   }
+// })()
