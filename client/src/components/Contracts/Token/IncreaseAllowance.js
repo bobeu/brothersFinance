9a6023fedb2f031411f2dd13016be707d94251ae
bobeu <bobmatea27@gmail.com>
2021-07-15 12:14:07 +0100

    updated

---
+++ client/src/components/Contracts/Token/IncreaseAllowance.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import tokenCmon from "../../Common/tokenCmon";
+import Result from "../../Common/Result";
+
+function IncreaseAllowance(props) {
+    const [ results, setErrors ] = useState({result_: Boolean,});
+
+    useEffect(() => {
+        ( async () => {
+            exec();
+        })()
+    }, []);
+    
+    function exec() {
+        const s = new Promise(
+            async (resolve, reject) => {
+                try{
+                    const web3 = tokenCmon();
+                    const account = await web3.account;
+                    const result = await web3.contract_instance.methods.increaseAllowance(props.target).send({from: account});
+                    resolve(result);
+                  
+
+                    setErrors({result_: result});
+                    
+                } catch (error) {
+                    reject(error);
+                }
+        })();
+    }
+
+    return(
+        <Result results={results}/>
+    );
+}
+    
+
+export default IncreaseAllowance;
